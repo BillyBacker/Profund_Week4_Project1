692a03b2d0d5974df668e050224fcd39459e7ff8
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-09-05 09:12:43 +0700

    Add project files.

---
+++ Profund_Week4_Project1/Source.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	unsigned long long In;
+	vector<char> Out;
+	char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E' , 'F'};
+	scanf("%lld", &In);
+	if (In == 0, In == 1) {
+		Out.push_back(hex[In]);
+	}
+	while(In > 1) {
+		Out.push_back(hex[In % 16]);
+		In /= 16;
+	}
+	for (int i = 0; i < Out.size(); i++) {
+		printf("%c", Out[Out.size()-1-i]);
+	}
+}
