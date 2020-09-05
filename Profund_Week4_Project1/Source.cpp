2708c1849a8c924fe878a531843597e8740f275a
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-09-05 13:28:53 +0700

    Fix bug when use 2 digits number.

--- Profund_Week4_Project1/Source.cpp
+++ Profund_Week4_Project1/Source.cpp
@@ -8,11 +8,13 @@ int main() {
 	unsigned long long In;
 	vector<char> Out;
 	char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E' , 'F'};
+	printf("Enter Decimal number : ");
 	scanf("%lld", &In);
+	printf("Hexadecimal number : ");
 	if (In == 0, In == 1) {
 		Out.push_back(hex[In]);
 	}
-	while(In > 1) {
+	while(In > 0) {
 		Out.push_back(hex[In % 16]);
 		In /= 16;
 	}
